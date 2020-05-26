80fc67df14adf1157a55e2c325bf4225dcb83bf0
Alex Grozav <alex@grozav.com>
2020-05-26 15:38:46 +0300

    Left gh pages in docs.

---
+++ docs/assets/js/8.22a43c22.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{258:function(t,n,e){"use strict";e.r(n);var s={name:"Button"},a=e(28),u=Object(a.a)(s,(function(){var t=this.$createElement;return(this._self._c||t)("a",{staticClass:"button"},[this._t("default")],2)}),[],!1,null,null,null);n.default=u.exports}}]);
