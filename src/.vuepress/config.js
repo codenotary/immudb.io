df745692816be5179b5fcfa24992c3656ad67893
Alex Grozav <alex@grozav.com>
2020-05-28 20:32:08 +0300

    Updated base url.

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    base: '/docs/',
+    base: '/',
     dest: 'docs',
 	title: 'immudb',
 	description: 'The lightweight, high-speed immutable database for systems and applications.',
