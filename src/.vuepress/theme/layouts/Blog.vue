2eeaf93b484682fc6f754251fe42734976bc0f04
Alex Grozav <alex@grozav.com>
2020-06-05 09:49:10 +0300

    Added blog post and blog listing layouts.

--- src/.vuepress/theme/layouts/Blog.vue
+++ src/.vuepress/theme/layouts/Blog.vue
@@ -1,131 +1,75 @@
 <template>
-    <div class="theme-container" :class="pageClasses"
-        @touchstart="onTouchStart"
-        @touchend="onTouchEnd">
+    <div id="blog" class="theme-container" :class="pageClasses" @touchstart="onTouchStart" @touchend="onTouchEnd">
         <Navbar v-if="shouldShowNavbar" @toggle-sidebar="toggleSidebar"/>
 
-        <i-container class="_margin-top-2">
+        <div class="sidebar-mask" @click="toggleSidebar(false)" />
+
+        <Sidebar :items="sidebarItems" @toggle-sidebar="toggleSidebar">
+            <template #top>
+                <slot name="sidebar-top" />
+            </template>
+            <template #bottom>
+                <slot name="sidebar-bottom" />
+            </template>
+        </Sidebar>
+
+        <i-container id="blog-post">
             <i-row>
                 <i-column>
-                    <h1>Blog</h1>
-                    <ul id="default-layout">
-                        <li v-for="page in $pagination.pages">
-                            <router-link class="page-link" :to="page.path">{{ page.title }}</router-link>
-                        </li>
-                    </ul>
-                    <div id="pagination">
-                        <router-link v-if="$pagination.hasPrev" :to="$pagination.prevLink">Prev</router-link>
-                        <router-link v-if="$pagination.hasNext" :to="$pagination.nextLink">Next</router-link>
-                    </div>
+                    <h1 class="blog-post-title">immudb</h1>
+                    <p class="blog-post-excerpt">{{ $description }}</p>
                 </i-column>
             </i-row>
         </i-container>
+
+        <i-container id="blog-posts" class="_margin-top-4">
+            <i-row id="default-layout">
+                <template v-for="page in $pagination.pages">
+                    <i-column lg="4" md="6">
+                        <i-card class="blog-entry">
+                            <router-link slot="image" class="blog-entry-image" :to="page.path">
+                                <img class="image -fluid" :src="$withBase(page.frontmatter.image)" :alt="page.frontmatter.title" />
+                            </router-link>
+                            <router-link class="blog-entry-title" :to="page.path">
+                                <h2>{{ page.frontmatter.title }}</h2>
+                            </router-link>
+                            <p class="blog-entry-meta">{{ date(page.frontmatter.date) | dateFormat('MMMM DD, YYYY') }}</p>
+                            <p class="blog-entry-description">{{ page.frontmatter.excerpt }}</p>
+                        </i-card>
+                    </i-column>
+                </template>
+            </i-row>
+            <i-row>
+                <i-column class="_text-center">
+                    <ul class="list -inline" id="pagination" v-if="$pagination.hasPrev || $pagination.hasNext">
+                        <li v-if="$pagination.hasPrev">
+                            <router-link :to="$pagination.prevLink">Prev</router-link>
+                        </li>
+                        <li v-if="$pagination.hasNext">
+                            <router-link :to="$pagination.nextLink">Next</router-link>
+                        </li>
+                    </ul>
+                </i-column>
+            </i-row>
+        </i-container>
+        <Footer />
     </div>
 </template>
 
 <script>
-import Home from '@theme/components/Home.vue'
-import Navbar from '@theme/components/Navbar.vue'
-import Page from '@theme/components/Page.vue'
-import Sidebar from '@theme/components/Sidebar.vue'
-import { resolveSidebarItems } from '../util'
+import Layout from '@theme/layouts/Layout.vue'
+import Footer from '@theme/components/Footer.vue'
 
 export default {
-  name: 'Layout',
-
-  components: {
-    Home,
-    Page,
-    Sidebar,
-    Navbar
-  },
-
-  data () {
-    return {
-      isSidebarOpen: false
-    }
-  },
-
-  computed: {
-    shouldShowNavbar () {
-      const { themeConfig } = this.$site
-      const { frontmatter } = this.$page
-      if (
-        frontmatter.navbar === false
-        || themeConfig.navbar === false) {
-        return false
-      }
-      return (
-        this.$title
-        || themeConfig.logo
-        || themeConfig.repo
-        || themeConfig.nav
-        || this.$themeLocaleConfig.nav
-      )
+    name: 'BlogPostLayout',
+    extends: Layout,
+    components: {
+        Footer
     },
-
-    shouldShowSidebar () {
-      const { frontmatter } = this.$page
-      return (
-        !frontmatter.home
-        && frontmatter.sidebar !== false
-        && this.sidebarItems.length
-      )
-    },
-
-    sidebarItems () {
-      return resolveSidebarItems(
-        this.$page,
-        this.$page.regularPath,
-        this.$site,
-        this.$localePath
-      )
-    },
-
-    pageClasses () {
-      const userPageClass = this.$page.frontmatter.pageClass
-      return [
-        {
-          'no-navbar': !this.shouldShowNavbar,
-          'sidebar-open': this.isSidebarOpen,
-          'no-sidebar': !this.shouldShowSidebar
-        },
-        userPageClass
-      ]
-    }
-  },
-
-  mounted () {
-    this.$router.afterEach(() => {
-      this.isSidebarOpen = false;
-    })
-  },
-
-  methods: {
-    toggleSidebar (to) {
-      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
-      this.$emit('toggle-sidebar', this.isSidebarOpen)
-    },
-
-    // side swipe
-    onTouchStart (e) {
-      this.touchStart = {
-        x: e.changedTouches[0].clientX,
-        y: e.changedTouches[0].clientY
-      }
-    },
-
-    onTouchEnd (e) {
-      const dx = e.changedTouches[0].clientX - this.touchStart.x
-      const dy = e.changedTouches[0].clientY - this.touchStart.y
-      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-        if (dx > 0 && this.touchStart.x <= 80) {
-          this.toggleSidebar(true)
-        } else {
-          this.toggleSidebar(false)
+    methods: {
+        date(date) {
+            return new Date(date);
         }
-      }
     }
-  }
 }
 </script>
