8fa1731fd4998602469e665ed4220d4277524c97
Alex Grozav <alex@grozav.com>
2020-06-05 09:48:27 +0300

    Added reusable footer component.

---
+++ src/.vuepress/theme/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="footer">Copyright &copy; vChain {{ year }}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            year: (new Date()).getFullYear()
+        };
+    }
+}
+</script>
