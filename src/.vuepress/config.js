924aa95f38b7a1a8d927f499bba29eac66015914
Alex Grozav <alex@grozav.com>
2020-07-23 17:12:58 +0300

    Added tawk.

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -32,6 +32,16 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-168112067-1');`;
 
+const tawkTo = `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5f1966f77258dc118beec8d6/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();`;
+
 module.exports = {
     base: '/',
     dest: 'docs',
@@ -64,6 +74,7 @@ module.exports = {
         ['meta', { name: "HandheldFriendly", content: "True" }],
         ['meta', { name: "viewport", content: "width=device-width, initial-scale=1.0" }],
         ['script', { type: "application/ld+json" }, schemaOrg],
+        ['script', { type: "text/javascript" }, tawkTo],
     ].concat(process.env.NODE_ENV === 'production' ? [
         ['script', { async: true, src: "https://www.googletagmanager.com/gtag/js?id=UA-168112067-1" }],
     ] : []),
