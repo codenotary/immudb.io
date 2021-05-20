e59dd6d8e082a7e6cbcf63f9c734e9bbfc06bc66
Artem Martynyuk <scarg.dev@gmail.com>
2021-05-20 17:39:38 +0200

    feat(WrappedSection): add component that allows to wrap sections according to mockup

---
+++ src/.vuepress/theme/global-components/WrappedSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapped-section">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WrappedSection"
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapped-section
+  border-radius $cn-border-radius-lg
+  box-shadow $cn-shadow-sm
+  background-color: $cn-color-primary_dark
+  padding 40px
+  h2
+    margin-top 'calc(-%s - 57px)' % ($navbarHeight)
+    color white
+    text-transform uppercase
+  p
+    margin-bottom 0
+</style>
