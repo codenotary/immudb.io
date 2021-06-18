df560851c9b9af530d14f7f79aa8fc1fcc3d5e68
Artem Martynyuk <scarg.dev@gmail.com>
2021-06-18 15:54:45 +0200

    feat(Layout): update spacings and H3 style inside WrappedSections

--- src/.vuepress/theme/global-components/WrappedSection.vue
+++ src/.vuepress/theme/global-components/WrappedSection.vue
@@ -23,6 +23,10 @@ export default {
 		margin-top 'calc(-%s - 57px)' % ($navbarHeight)
 		color white
 		text-transform uppercase
+	& > h3
+		padding-top 'calc(%s + 57px)' % ($navbarHeight)
+		margin-top -120px
+		color $cn-color-secondary
 	p
 		margin-bottom 0
 	& > p > code
