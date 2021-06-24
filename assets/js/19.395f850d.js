a1daff6bb8b3d4bda479f4343cb8a7ba326366b9
Temii <Temii@users.noreply.github.com>
2021-06-24 13:20:35 +0000

    deploy: ff262dd0b4e44501636989870c6c9b78acf6f84c

--- assets/js/19.395f850d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{453:function(e,t,n){"use strict";n.r(t);n(25),n(23);var a=n(62),c={name:"RedirectToDefaultVersion",beforeMount:function(){this.$router.replace({path:Object(a.c)()})}},r=n(10),o=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"3a5aaa7c",null);t.default=o.exports}}]);
