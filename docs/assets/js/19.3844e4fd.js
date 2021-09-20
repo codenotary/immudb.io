eb9be49d227dc60fe906c24f2bddfd7d1a0574a5
Hextar13 <ste13456@gmail.com>
2021-09-20 10:13:37 +0000

    Updated documentation and images.

--- docs/assets/js/19.3844e4fd.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{548:function(e,t,n){"use strict";n.r(t);n(27),n(31);var a=n(74),c={name:"RedirectToDefaultVersion",beforeMount:function(){this.$router.replace({path:Object(a.c)()})}},r=n(11),o=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"3a5aaa7c",null);t.default=o.exports}}]);
