b88e8e0f5af1328263728d4a1f2e48b54288d929
Hextar13 <ste13456@gmail.com>
2021-09-23 10:51:28 +0200

    chore: regenerated package-lock.json

--- predeploy.sh
+++ predeploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rm -fr node_modules package-lock.json && npm cache clear && npm i -f && npm add --all && npm commit -m "chore: regenerated package-lock.json"
+rm -fr node_modules package-lock.json && npm cache clean -f && npm i -f && git add --all && git commit -m "chore: regenerated package-lock.json"
