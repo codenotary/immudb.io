23dd9849c43ce5b4fd2b58977b6a962fbb6dac39
Hextar13 <ste13456@gmail.com>
2021-11-04 15:27:44 +0100

    chore: updated PR GA

--- docs/assets/js/20.764a31c7.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{606:function(e,t,n){"use strict";n.r(t);n(32),n(37);var a=n(84),c={name:"RedirectToDefaultVersion",beforeMount:function(){this.$router.replace({path:Object(a.c)()})}},r=n(17),o=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"3a5aaa7c",null);t.default=o.exports}}]);
