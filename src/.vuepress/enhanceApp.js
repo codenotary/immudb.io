5651c38e353873d00695f9889c83aea9189f2b8b
Zaza <zanda.andre@gmail.com>
2021-12-10 17:53:42 +0100

    update and delete section (#100)
    * delete section
    
    * keeping only delete
    
    * upsert
    
    * delete description

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -121,7 +121,10 @@ const getSidebar = version => {
   };
 
   switch (version) {
-    case '/master':
+    case '/master': {
+      develop.children.push(`${version}/develop/deleting`);
+  }
+
     case '/1.1.0':
 	  case '/1.0.0':
     {
