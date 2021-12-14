abf32c2f1f73b49e903455a38de6099df26a3d7e
Zaza <zanda.andre@gmail.com>
2021-12-14 15:29:36 +0100

    release 1.2.1 (#103)
    * release 1.2.1

--- src/.vuepress/theme/util/index.js
+++ src/.vuepress/theme/util/index.js
@@ -239,7 +239,7 @@ function resolveItem (item, pages, base, groupDepth = 1) {
   }
 }
 
-export const versions = ['0.8.1', '0.9.2', '1.0.0', '1.1.0', 'master']
+export const versions = ['0.8.1', '0.9.2', '1.0.0', '1.1.0','1.2.1', 'master']
 
 export function getVersionFromRoute (route = { fullPath: '' }) {
   const matches = route.fullPath.match(/[^\/]+/)
