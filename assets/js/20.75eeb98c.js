1ff4f95887754ba48a6e4e35a39bc306fec4740c
Hextar <Hextar@users.noreply.github.com>
2022-01-11 10:31:52 +0000

    deploy: 4e904957153d85e7360e655e2ff8de836787bcf3

--- assets/js/20.75eeb98c.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{615:function(e,t,n){"use strict";n.r(t);n(23),n(39);var a=n(83),c={name:"RedirectToDefaultVersion",beforeMount:function(){this.$router.replace({path:Object(a.c)()})}},r=n(18),o=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"3a5aaa7c",null);t.default=o.exports}}]);
