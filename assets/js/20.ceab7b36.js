11d54ad66fbe1ba256982ab0e55ee8a6b12425a3
rafaelveggi <rafaelveggi@users.noreply.github.com>
2022-01-31 12:19:33 +0000

    deploy: 4c11ee7144ca9021a0693a67ef84040a7ee00fee

--- assets/js/20.ceab7b36.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{626:function(e,t,n){"use strict";n.r(t);n(22),n(43);var a=n(86),c={name:"RedirectToDefaultVersion",beforeMount:function(){this.$router.replace({path:Object(a.c)()})}},r=n(19),o=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"3a5aaa7c",null);t.default=o.exports}}]);
