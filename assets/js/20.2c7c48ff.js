5f94529cd464820092adb23e12906aebe2bf97d3
byo <byo@users.noreply.github.com>
2022-06-22 10:03:05 +0000

    deploy: 8bb9c8eb715bc11681ec179c77df387c7b703f3c

--- assets/js/20.2c7c48ff.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{530:function(e,t,n){"use strict";n.r(t);n(19),n(36);var a=n(32),c={name:"RedirectToDefaultVersion",beforeMount:function(){this.$router.replace({path:Object(a.c)()})}},r=n(14),o=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"3a5aaa7c",null);t.default=o.exports}}]);
