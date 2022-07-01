c97345a73efb333aaf50581dcb97707f22e7d973
jeroiraz <jeroiraz@users.noreply.github.com>
2022-07-01 12:41:20 +0000

    deploy: 05e1c08430f97bc553fd5b561905046ed0fc756d

---
+++ assets/js/20.ee7a77d6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{531:function(e,t,n){"use strict";n.r(t);n(20),n(37);var a=n(19),c={name:"RedirectToDefaultVersion",beforeMount:function(){this.$router.replace({path:Object(a.c)()})}},r=n(14),o=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"3a5aaa7c",null);t.default=o.exports}}]);
