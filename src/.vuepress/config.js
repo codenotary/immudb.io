a28b1ae9240d0d627904466c61270ef64abb730a
Simone Lazzaris <SimoneLazzaris@users.noreply.github.com>
2023-06-12 09:50:25 +0200

    Update config.js
    Set production code for google analytics

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -33,7 +33,7 @@ const schemaOrg = `{
 const googleAnalytics = `window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'UA-168112067-1');`;
+gtag('config', 'G-ELLNP48DRV');`;
 
 const vgo = `(function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
 vgo('setAccount', '66487182');
